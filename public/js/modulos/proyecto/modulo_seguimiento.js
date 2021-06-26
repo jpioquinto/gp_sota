ba8fc35a1ac9321ef2b37eff0c4880161f5c2cd9
Jesús Pioquinto <jes.pioquinto@gmail.com>
2021-06-25 19:05:55 -0500

    Sigue el desarrollo del módulo seguimiento, se inicia el maquetado de galería de fotos y videos.

--- public/js/modulos/proyecto/modulo_seguimiento.js
+++ public/js/modulos/proyecto/modulo_seguimiento.js
@@ -4,10 +4,18 @@ var $modSeguimiento = (modulo=>{
 })($modSeguimiento || {});
 
 $(function() {
-    $('#jq_listado_acciones').DataTable({
+    $('#jq_listado_acciones').DataTable({  
+        'rowsGroup': [0],     
         "pageLength": 50,
         language: {
             url: 'js/plugin/datatables/Spanish_Mexico.json'
-        }
+        }/*, ,
+        'columnDefs': [
+            {
+               'targets': [1, 2, 3, 4, 5],
+               'orderable': true,
+               'searchable': true
+            }
+        ]*/
     });
 });
