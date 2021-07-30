13276807b9dc9f2debec01e1c8b996e11774b77e
Jesús Pioquinto <jes.pioquinto@gmail.com>
2021-07-30 12:37:46 -0500

    Agregando nueva plantilla para el login con todo y sus recursos, css, fonts.

---
+++ app/Views/login/v_login_v1.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="es-mx">
+<head>
+	<title>Seguimiento para Proyectos del Ramo | SOTA</title>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="IE=9" >
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+	<meta name="keywords" content="Proyectos del ramo, subsecretaría de ordenamiento territorial y agrario.">
+	<meta name="description" content="Seguimiento de proyectos">
+	<meta name="robots" content="none|index|follow">
+	<link href='images/favicon/favicon.png' rel='shortcut icon'>
+	<link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Open+Sans:600'>
+    <link rel="stylesheet" href="css/login/login.css">
+	<link rel="stylesheet" href="css/plugins/animate.min.css"/>
+	<!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"-->
+<style>
+.hide{
+	opacity:0;
+}
+</style>
+</head>
+<body>
+	<div class="login-wrap">
+		<div class="login-html">
+			<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Iniciar Sesión</label>
+			<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab" style="background: #9d2449;color: #fff;border-radius: 2px;padding: 6px;">¿Olvidaste tu contraseña?</label>
+			<!--input id="tab-3" type="radio" name="tab" class="sign-down"><label for="tab-3" class="tab">Historial de cambios</label-->
+			
+			<div class="login-form">
+				<div class="sign-in-htm">
+				
+					<img src="images/logos/sedatu.png" width="385px"/>
+					<h4 style="text-align:center;margin-top:0px;font-size:16px;color:#c5004a;">Seguimiento para Proyectos del Ramo.</h4>
+					<div class="group">
+					<label for="user" class="label"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 513.32 513.32" style="enable-background:new 0 0 513.32 513.32;" xml:space="preserve">
+						<path d="M346.491,316.547c49.193-29.944,81.275-83.414,81.275-145.44C427.767,76.998,350.768,0,256.66,0
+							S85.553,76.998,85.553,171.107c0,62.026,32.082,115.497,81.275,145.44C81.275,348.63,17.11,423.489,0,513.32h42.777
+							c21.388-98.386,109.081-171.107,213.883-171.107s192.495,72.72,213.883,171.107h42.777
+							C496.21,421.35,432.045,346.491,346.491,316.547z M128.33,171.107c0-70.581,57.749-128.33,128.33-128.33
+							s128.33,57.749,128.33,128.33s-57.749,128.33-128.33,128.33S128.33,241.688,128.33,171.107z"/>
+					</svg></label>
+						<input autocomplete="off" id="usuario" type="text" class="input" placeholder="Usuario">
+					</div>
+					<div class="group">
+						<label for="pass" class="label"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+							 viewBox="0 0 511 511" style="enable-background:new 0 0 511 511;" xml:space="preserve">
+							<path d="M479.5,132h-448C14.131,132,0,146.131,0,163.5v96C0,276.869,14.131,291,31.5,291h280c4.142,0,7.5-3.358,7.5-7.5
+								s-3.358-7.5-7.5-7.5h-280c-9.098,0-16.5-7.402-16.5-16.5v-96c0-9.098,7.402-16.5,16.5-16.5h448c9.098,0,16.5,7.402,16.5,16.5v72
+								c0,4.142,3.358,7.5,7.5,7.5s7.5-3.358,7.5-7.5v-72C511,146.131,496.869,132,479.5,132z"/>
+							<path d="M479,253.234V243.5c0-35.014-28.486-63.5-63.5-63.5S352,208.486,352,243.5v9.734c-9.29,3.138-16,11.93-16,22.266v80
+								c0,12.958,10.542,23.5,23.5,23.5h112c12.958,0,23.5-10.542,23.5-23.5v-80C495,265.164,488.29,256.372,479,253.234z M415.5,195
+								c26.743,0,48.5,21.757,48.5,48.5v8.5h-97v-8.5C367,216.757,388.757,195,415.5,195z M480,355.5c0,4.687-3.813,8.5-8.5,8.5h-112
+								c-4.687,0-8.5-3.813-8.5-8.5v-80c0-4.687,3.813-8.5,8.5-8.5h112c4.687,0,8.5,3.813,8.5,8.5V355.5z"/>
+							<path d="M121.708,191.75c-2.071-3.588-6.658-4.816-10.245-2.745L95,198.51V179.5c0-4.142-3.358-7.5-7.5-7.5s-7.5,3.358-7.5,7.5
+								v19.01l-16.463-9.505c-3.587-2.072-8.174-0.842-10.245,2.745c-2.071,3.587-0.842,8.174,2.745,10.245L72.5,211.5l-16.463,9.505
+								c-3.587,2.071-4.816,6.658-2.745,10.245c1.389,2.406,3.91,3.751,6.502,3.751c1.272,0,2.562-0.324,3.743-1.006L80,224.49v19.01
+								c0,4.142,3.358,7.5,7.5,7.5s7.5-3.358,7.5-7.5v-19.01l16.463,9.505c1.181,0.682,2.471,1.006,3.743,1.006
+								c2.592,0,5.113-1.345,6.502-3.751c2.071-3.587,0.842-8.174-2.745-10.245L102.5,211.5l16.463-9.505
+								C122.55,199.924,123.779,195.337,121.708,191.75z"/>
+							<path d="M217.708,191.75c-2.071-3.588-6.658-4.816-10.245-2.745L191,198.51V179.5c0-4.142-3.358-7.5-7.5-7.5s-7.5,3.358-7.5,7.5
+								v19.01l-16.463-9.505c-3.587-2.072-8.174-0.842-10.245,2.745c-2.071,3.587-0.842,8.174,2.745,10.245L168.5,211.5l-16.463,9.505
+								c-3.587,2.071-4.816,6.658-2.745,10.245c1.389,2.406,3.91,3.751,6.502,3.751c1.272,0,2.562-0.324,3.743-1.006L176,224.49v19.01
+								c0,4.142,3.358,7.5,7.5,7.5s7.5-3.358,7.5-7.5v-19.01l16.463,9.505c1.181,0.682,2.471,1.006,3.743,1.006
+								c2.592,0,5.113-1.345,6.502-3.751c2.071-3.587,0.842-8.174-2.745-10.245L198.5,211.5l16.463-9.505
+								C218.55,199.924,219.779,195.337,217.708,191.75z"/>
+							<path d="M313.708,191.75c-2.071-3.588-6.658-4.816-10.245-2.745L287,198.51V179.5c0-4.142-3.358-7.5-7.5-7.5s-7.5,3.358-7.5,7.5
+								v19.01l-16.463-9.505c-3.587-2.072-8.174-0.842-10.245,2.745c-2.071,3.587-0.842,8.174,2.745,10.245L264.5,211.5l-16.463,9.505
+								c-3.587,2.071-4.816,6.658-2.745,10.245c1.389,2.406,3.91,3.751,6.502,3.751c1.272,0,2.562-0.324,3.743-1.006L272,224.49v19.01
+								c0,4.142,3.358,7.5,7.5,7.5s7.5-3.358,7.5-7.5v-19.01l16.463,9.505c1.181,0.682,2.471,1.006,3.743,1.006
+								c2.592,0,5.113-1.345,6.502-3.751c2.071-3.587,0.842-8.174-2.745-10.245L294.5,211.5l16.463-9.505
+								C314.55,199.924,315.779,195.337,313.708,191.75z"/>
+							<path d="M415.5,284c-12.958,0-23.5,10.542-23.5,23.5c0,10.336,6.71,19.128,16,22.266v9.734c0,4.142,3.358,7.5,7.5,7.5
+								s7.5-3.358,7.5-7.5v-9.734c9.29-3.138,16-11.93,16-22.266C439,294.542,428.458,284,415.5,284z M415.5,316
+								c-4.687,0-8.5-3.813-8.5-8.5s3.813-8.5,8.5-8.5s8.5,3.813,8.5,8.5S420.187,316,415.5,316z"/>
+						</svg></label>
+						<input autocomplete="off" id="password" type="password" class="input" data-type="password" placeholder="Contraseña">
+                        <strong href="#" id="verContra" style="margin-left:50px;font-size:12px;cursor:pointer;position:relative;top:-35px;right:-300px;"><img class="hide iconoPassword" width="20px" src="images/iconos/svg/observe.svg"/></strong>
+					</div>
+					<!-- sección del captcha -->
+					<!--p style="font-size: 13px;margin-bottom:-6px;">Haga clic para cambiar el código</p>
+					<br>
+
+					<div style="vertical-align: top;text-align: center;" id="imagenCaptchar" class="group text-center div-captchar">
+						<div style="width:40%;vertical-align: top;display: inline-block;cursor: pointer;" class='divImagen'>
+							<?=isset($captcha) ? $captcha : ''?>
+						</div>
+						<div style="width: 57%;vertical-align: top;display: inline-block;">
+							<input autocomplete="off" type="text" name="captcha" id="captcha" class="input" maxlength="5" placeholder="Ingrese el código" style="width: 100%;">
+						</div>	
+					</div-->
+					<br>
+
+					<div class="group" style="margin-top:-35px;">
+						<button id="btn_logino" class="button"> Ingresar</button>
+					</div>
+					<div class="group" style="display: none;">
+						<a href="javascript:;" class="jq_ver_historial">Historial de cambios</a>
+					</div>
+
+				</div>
+				<div class="sign-up-htm">
+					<div class="login-box-plain">
+						<h2 class="bigintro"> Recupera tu cuenta</h2>
+						<div class="divide-10"></div>
+						<form id="formReset">
+							<div class="form-actions">
+						  		<p style="text-align:justify;">Ingrese su usuario o dirección de correo que registro en su perfil, le enviaremos un correo electrónico con un enlace para restablecer su contraseña, por seguridad se recomienda cambiarla por una que pueda recordar fácilmente.</p>
+						  		<div class="divide-10"></div>
+						  		<div class="row">
+							  		<div class="form-group col-md-12">
+										<div class="group col-md-12">
+											<input id="usuarioReset" type="text" class="input" placeholder="Usuario o correo electónico" style="width:100%!important;">
+										</div>
+								  	</div>
+								  	<div class="col-md-12 group">
+									  	<button type="button" id="btn_reset" class="button btn-success not-allowed btn disabled" disabled>Enviar correo de recuperación</button>
+									</div>
+								</div>
+								<div class="alert alert-block fade in hidden">
+									<h4><i class="fa"></i><span>&nbsp;</span></h4>
+								</div>
+						  	</div>
+						</form>
+					</div>
+				</div>				
+			</div>
+		</div>
+		<div class="historial-html" style="display: none;background: #FFFFFF;">
+			<?=(isset($v_historial))?$v_historial:""?>
+		</div>
+		<?= csrf_field() ?>
+	</div>
+	<script src="js/library/jquery-3.6.0.min.js"></script>
+	<script src="js/app/jquery.noty.packaged.min.js"></script>
+	<script src="js/helpers.js"></script>
+	<script src="js/util.js"></script>	
+	<script src="js/modulos/login/login.js"></script>
+</body>
+</html>
